df8783adb15fa25581cbad794e4e0c8e3e6f0858
Greg Schechter <diabolomaster@gmail.com>
2016-02-18 15:39:20 -0800

    Get user data after auth

--- src/Base.js
+++ src/Base.js
@@ -2,32 +2,55 @@ var React = require('react');
 var Header = require('./Header');
 var Firebase = require('firebase');
 var firebaseRef = new Firebase('https://blazing-inferno-9225.firebaseio.com/');
+var ReactFireMixin = require('reactfire');
 
 var Base = React.createClass({
+    mixins: [ReactFireMixin],
+
     childContextTypes: {
         authData: React.PropTypes.object,
+        userData: React.PropTypes.object,
     },
 
     getChildContext: function() {
         return {
-            authData: this.state.authData
+            authData: this.state.authData,
+            userData: this.state.userData,
         };
     },
 
     getInitialState() {
         return {
             authData: null,
+            userData: null,
         };
     },
 
     componentDidMount() {
+        this.getUserData();
         firebaseRef.onAuth(this.onAuthCallback);
+    },
+
+    getUserData() {
+        if (this.state.authData) {
+            var ref = firebaseRef.child('users').child(this.state.authData.uid);
+            this.bound = true;
+            this.bindAsObject(ref, 'userData');
+        } else {
+            if (this.bound) {
+                this.unbind('userData');
+                this.bound = false;
+            }
+            this.setState({
+                userData: null,
+            });
+        }
     },
 
     onAuthCallback(authData) {
         this.setState({
             authData: authData,
-        });
+        }, this.getUserData);
     },
 
     componentDidUnMount() {
