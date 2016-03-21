74f423bf4b0007c5bf1f40fa8f5ac541177405d3
Greg Schechter <diabolomaster@gmail.com>
2016-03-21 12:55:26 -0700

    Make changes to run on heroku

--- src/Keys.js
+++ src/Keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     cryptr: process.env.cryptr,
     firebase: process.env.firebase,
-    postmark: process.env.POSTMARK_API_KEY,
+    postmark: process.env.POSTMARK_API_KEY || 'dummy',
     stripe: process.env.STRIPE_KEY,
 };
