fa3e9d38e142a922cfbc3968f60fd31c4c36d812
Greg Schechter <diabolomaster@gmail.com>
2016-03-21 10:00:38 -0700

    Add nightwatch tests

---
+++ nightwatch/basicTest.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'Test basic content renders': (browser) => {
+        var baseUrl = 'http://localhost:5000';
+        browser
+            .url(baseUrl)
+            .expect.element('.navbar-brand').text.to.contain('Birder Vs Birder');
+        browser
+            .end();
+    },
+};
