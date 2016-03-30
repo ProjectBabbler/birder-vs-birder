593c5f669a4ac261f4353705e37f1a288cfde7a5
Greg Schechter <diabolomaster@gmail.com>
2016-03-30 12:25:32 -0700

    Added donate button to email

---
+++ email/TestEmail.js
@@ -0,0 +1,12 @@
+var Layout = require('./Layout');
+var React = require('react');
+
+var TestEmail = (props) => {
+    return (
+        <Layout>
+            <h3>Some test content</h3>
+        </Layout>
+    );
+};
+
+module.exports = TestEmail;
