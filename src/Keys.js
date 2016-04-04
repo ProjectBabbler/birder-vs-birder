dce045d290ed1915f7c7f164aa02d12a0fe6bfe1
Greg Schechter <diabolomaster@gmail.com>
2016-04-04 16:46:08 -0700

    test some more

--- src/Keys.js
+++ src/Keys.js
@@ -3,8 +3,8 @@ module.exports = {
     firebase: process.env.firebase,
     postmark: process.env.POSTMARK_API_KEY || 'dummy',
     stripe: process.env.STRIPE_KEY,
-    ironcacheProject: process.env.IRON_CACHE_PROJECT,
-    ironcacheToken: process.env.IRON_CACHE_TOKEN,
+    ironcacheProject: process.env.IRON_CACHE_PROJECT || 'dummy',
+    ironcacheToken: process.env.IRON_CACHE_TOKEN || 'dummy',
     mailchimp: process.env.MAILCHIMP_API,
     newrelic: process.env.NEWRELIC,
 };
