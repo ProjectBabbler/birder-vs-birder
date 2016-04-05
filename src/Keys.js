b98aa8818f3d3d24c8aa68225ee3d265edf300c8
Greg Schechter <diabolomaster@gmail.com>
2016-04-05 10:51:43 -0700

    Use travis enviorment vars

--- src/Keys.js
+++ src/Keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     cryptr: process.env.cryptr,
     firebase: process.env.firebase,
-    postmark: process.env.POSTMARK_API_KEY || 'dummy',
+    postmark: process.env.POSTMARK_API_KEY,
     stripe: process.env.STRIPE_KEY,
-    ironcacheProject: process.env.IRON_CACHE_PROJECT || 'dummy',
-    ironcacheToken: process.env.IRON_CACHE_TOKEN || 'dummy',
+    ironcacheProject: process.env.IRON_CACHE_PROJECT,
+    ironcacheToken: process.env.IRON_CACHE_TOKEN,
     mailchimp: process.env.MAILCHIMP_API,
-    newrelic: process.env.NEWRELIC || 'dummy',
+    newrelic: process.env.NEWRELIC,
 };
