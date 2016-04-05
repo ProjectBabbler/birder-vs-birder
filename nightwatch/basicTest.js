ef81f8d84304dd2b90cd7a94348c4a797209e80a
Greg Schechter <diabolomaster@gmail.com>
2016-04-05 12:36:47 -0700

    Revert "Undue nginx"
    This reverts commit 0a1871644dad6248972737051e4ccec5aa3586e7.

--- nightwatch/basicTest.js
+++ nightwatch/basicTest.js
@@ -1,10 +1,10 @@
 module.exports = {
     'Test basic content renders': (browser) => {
-        var baseUrl = 'http://www.birdervsbirder.com:5000';
+        var baseUrl = 'http://localhost:5000';
 
         browser
             .url(baseUrl)
-            .expect.element('title').text.to.contain('Birder vs Birder').after(20000);
+            .expect.element('.navbar-brand').text.to.contain('Birder Vs Birder').after(20000);
         browser
             .end();
     },
