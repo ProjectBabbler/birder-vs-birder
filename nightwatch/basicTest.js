4e7d6ba40b5d92e559a7aad212224d712d312cb6
Greg Schechter <diabolomaster@gmail.com>
2016-04-05 15:57:55 -0700

    check title

--- nightwatch/basicTest.js
+++ nightwatch/basicTest.js
@@ -4,7 +4,7 @@ module.exports = {
 
         browser
             .url(baseUrl)
-            .expect.element('.navbar-brand').text.to.contain('Birder Vs Birder').after(50000);
+            .expect.element('title').text.to.contain('Birder vs Birder').after(2000);
         browser
             .end();
     },
