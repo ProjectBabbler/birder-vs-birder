ca736e068a92e15470de662e3c63e72880cabea1
Greg Schechter <diabolomaster@gmail.com>
2016-04-05 16:20:36 -0700

    Some attempts

--- nightwatch/basicTest.js
+++ nightwatch/basicTest.js
@@ -1,10 +1,9 @@
 module.exports = {
     'Test basic content renders': (browser) => {
-        var baseUrl = 'http://www.birdervsbirder.com';
+        var baseUrl = 'http://www-local.birdervsbirder.com';
 
         browser
             .url(baseUrl)
-            .waitForElementPresent('.navbar-brand', 20000)
             .expect.element('body').text.to.contain('Birder Vs Birder').after(2000);
         browser
             .end();
