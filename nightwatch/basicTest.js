c7c357a6f49bbd27ae805bd8fe0c436547cfde38
Greg Schechter <diabolomaster@gmail.com>
2016-04-07 18:03:42 -0700

    clean up tests

--- nightwatch/basicTest.js
+++ nightwatch/basicTest.js
@@ -4,12 +4,6 @@ module.exports = {
 
         browser
             .url(baseUrl)
-            .getLogTypes(function(result) {
-                console.log(result);
-            })
-            .getLog('browser', function(result) {
-                console.log(result);
-            })
             .expect.element('body').text.to.contain('Birder Vs Birder').after(20000);
         browser
             .end();
