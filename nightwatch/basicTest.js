a4a992ad851826ec0e42d20afff44577b125272c
Greg Schechter <diabolomaster@gmail.com>
2016-04-07 17:39:22 -0700

    Log errors

--- nightwatch/basicTest.js
+++ nightwatch/basicTest.js
@@ -4,6 +4,12 @@ module.exports = {
 
         browser
             .url(baseUrl)
+            .getLogTypes(function(result) {
+                console.log(result);
+            })
+            .getLog('browser', function(result) {
+                console.log(result);
+            })
             .expect.element('body').text.to.contain('Birder Vs Birder').after(20000);
         browser
             .end();
