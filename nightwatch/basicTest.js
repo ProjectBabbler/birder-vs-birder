edb147ee8e2f3fafa3d3649368c144a8ff9cc707
Greg Schechter <diabolomaster@gmail.com>
2016-04-07 18:11:47 -0700

    Basic Tests

---
+++ nightwatch/basicTest.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'Test basic content renders': (browser) => {
+        var baseUrl = 'http://www-local.birdervsbirder.com';
+
+        browser
+            .url(baseUrl)
+            .expect.element('body').text.to.contain('Birder Vs Birder').after(20000);
+        browser
+            .end();
+    },
+};
