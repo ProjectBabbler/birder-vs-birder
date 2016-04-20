bfd5956c8865f5c5ef1679c1493b953973c561e4
Greg Schechter <diabolomaster@gmail.com>
2016-04-19 23:09:30 -0700

    Move custom style out of js bundle and into own file

--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,5 @@
 import { render } from 'react-dom';
 require('babel-polyfill');
-require('../styles/CustomStyles.css');
-
 var routes = require('./routes');
 
 render(routes, document.getElementById('root'));
