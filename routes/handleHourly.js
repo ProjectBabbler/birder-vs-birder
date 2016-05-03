d374e768f8ad6202bc06029211e80bce0973d36f
Greg Schechter <gregthebusker@gmail.com>
2016-05-02 17:14:43 -0700

    only run every hour hours

---
+++ routes/handleHourly.js
@@ -0,0 +1,17 @@
+var ChallengesManager = require('../managers/ChallengesManager');
+var UsersManager = require('../managers/UsersManager');
+
+// Only run this on every fourth hour.
+var moment = require('moment');
+if (moment().hour() % 4 != 0) {
+    process.exit(0);
+} else {
+    ChallengesManager.updateCache().then(() => {
+        return UsersManager.updateCache();
+    }).then(() => {
+        process.exit(0);
+    }).catch(e => {
+        console.error(e);
+        process.exit(1);
+    });
+}
