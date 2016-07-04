152e460a09663bf8f6c1025f7b561735b880c673
Greg Schechter <diabolomaster@gmail.com>
2016-07-03 20:48:19 -0700

    Don't load node firebase reference anymore

--- react/firebase.js
+++ react/firebase.js
@@ -1,13 +1,9 @@
-if (process.env.BROWSER) {
-    var firebase = require('firebase');
-    var config = {
-        apiKey: 'AIzaSyAgN21NhwJt6MMLxanxxsgHR712fB6QjSE',
-        authDomain: 'blazing-inferno-9225.firebaseapp.com',
-        databaseURL: 'https://blazing-inferno-9225.firebaseio.com',
-    };
+var firebase = require('firebase');
+var config = {
+    apiKey: 'AIzaSyAgN21NhwJt6MMLxanxxsgHR712fB6QjSE',
+    authDomain: 'blazing-inferno-9225.firebaseapp.com',
+    databaseURL: 'https://blazing-inferno-9225.firebaseio.com',
+};
 
-    let firebaseApp = firebase.initializeApp(config, 'birder-vs-birder');
-    module.exports = firebaseApp;
-} else {
-    module.exports = require('../../firebaseNode');
-}
+let firebaseApp = firebase.initializeApp(config, 'birder-vs-birder');
+module.exports = firebaseApp;
