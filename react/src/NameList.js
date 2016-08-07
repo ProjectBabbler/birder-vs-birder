e51d9ec3f0ba848824efb824b68e3cfba86336e5
Greg Schechter <diabolomaster@gmail.com>
2016-08-06 21:51:46 -0700

    Added a add me to this list button

--- react/src/NameList.js
+++ react/src/NameList.js
@@ -1,12 +1,23 @@
 var React = require('react');
 var NameSearch = require('./NameSearch');
 var ButtonList = require('./ButtonList');
+var { Button } = require('react-bootstrap');
 
 var NameList = React.createClass({
+    contextTypes: {
+        authData: React.PropTypes.object,
+    },
+
     onNameAdded(obj) {
         let set = new Set(this.props.list);
         set.add(obj.value);
         this.props.onChange(set);
+    },
+
+    addMe() {
+        this.onNameAdded({
+            value: this.context.authData.uid,
+        });
     },
 
     render() {
@@ -14,6 +25,11 @@ var NameList = React.createClass({
             <div>
                 <NameSearch onChange={this.onNameAdded} />
                 <ButtonList style={{ marginTop: 30 }} list={this.props.list} onChange={this.props.onChange} />
+                {this.context.authData && !this.props.list.has(this.context.authData.uid) ? (
+                    <Button style={{ marginTop: 30 }} onClick={this.addMe}>
+                        Add me to this list
+                    </Button>
+                ) : null}
             </div>
         );
     },
