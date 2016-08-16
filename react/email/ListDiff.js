70c0b042d24cfa829f2e049bf3382864dbdaa120
Greg Schechter <gregthebusker@gmail.com>
2016-08-15 19:57:24 -0700

    Don't truncate list diff

--- react/email/ListDiff.js
+++ react/email/ListDiff.js
@@ -10,18 +10,11 @@ var ListDiff = (props) => {
     sortedList = sortedList.sort((a, b) => {
         return new Date(b.date) - new Date(a.date);
     });
-    sortedList = sortedList.splice(0, Math.min(props.diff, 3)).map(s => {
+    sortedList = sortedList.map(s => {
         return (
             <div key={s.commonName} style={{whiteSpace: 'nowrap'}}>{s.commonName}</div>
         );
     });
-    if (props.diff > 3) {
-        if (props.diff == 4) {
-            sortedList.push('plus 1 other');
-        } else {
-            sortedList.push(`plus ${props.diff - 3} others`);
-        }
-    }
     return (
         <div>
             {sortedList}
