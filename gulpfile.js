bcb9a7857466cfd571cc12cfc163b2ed18f91a95
Greg Schechter <gregthebusker@gmail.com>
2018-03-24 08:01:29 -0700

    Remove gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var webpack = require('webpack');
 var babel = require('gulp-babel');
 
@@ -14,9 +13,6 @@ gulp.task('compile-js', () => {
 
 gulp.task('webpack', (callback) => {
     webpack(require('./webpack.config.prod.js'), (err, stats) => {
-        if (err) {
-            throw new gutil.PluginError('webpack', err);
-        }
         gutil.log('[webpack]', stats.toString({
             chunkModules: false,
         }));
